036e81b1189e2031f10e056ae21e14b0e1f1e2f9
Dylan Katz <dykatz@uw.edu>
2020-12-15 17:09:33 -0800

    Add events to YahtzeeSlot

--- src/YahtzeeSlot.svelte
+++ src/YahtzeeSlot.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte';
     import { ready } from './stores';
     
+    let dispatch = createEventDispatcher();
+
     export let score: number = 0;
     export let displayScore: number = 0;
 
@@ -13,7 +16,8 @@
             burned = true;
         }
         $ready = false;
+        dispatch('assignment');
     }
 </script>
 
-<button {disabled} on:click={onClick}>{displayScore}</button>
+<button {disabled} on:click={onClick}>{disabled ? score : displayScore}</button>
