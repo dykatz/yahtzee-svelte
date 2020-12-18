89421a75773419845db891e446d92aeee0645e41
Dylan Katz <dykatz@uw.edu>
2020-12-17 17:18:41 -0800

    Add restarting the game upon completion

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import DiceBar from './DiceBar.svelte';
 	import ScoreBoard from './ScoreBoard.svelte';
+	import Leaderboard from './Leaderboard.svelte';
 </script>
 
 <main>
 	<ScoreBoard/>
 	<DiceBar/>
+	<Leaderboard/>
 </main>
---
+++ src/Leaderboard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { usedSlots } from './stores';
+
+    let disabled: boolean = true;
+    let nickName: string = "";
+
+    $: if ($usedSlots === 13) {
+        disabled = false;
+    }
+
+    function onSave() {
+        disabled = true;
+        $usedSlots = 0;
+        nickName = "";
+    }
+</script>
+
+<input type="text" autocomplete="off" disabled={disabled} value={nickName}>
+<button on:click="{onSave}" disabled={disabled}>Submit</button>
